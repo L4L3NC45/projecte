663f76b89c96aff9e013f78a9ef1149d5406a3dc
Matthew <mateoo2335@gmail.com>
2020-03-01 06:41:09 +0100

    He ficat algunes cosetes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './header';
 import Footer from './footer';
-import Button from 'react-bootstrap/Button';
+//import { Button } from '../node_modules/react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap/';
+
 
 class App extends React.Component{
 
@@ -18,13 +20,24 @@ class App extends React.Component{
     <Header/>
       <Router>
         <nav className="nav-bar">
-          <Link id="home" to="/">Home</Link>
-          <Link id="reg" to="Reg">Register</Link>
+          <Navbar bg="dark" variant="dark">
+          <Link className="Link" id="home" to="/"><Navbar.Brand id="navlog">Game</Navbar.Brand></Link>
+            <Nav className="mr-auto">
+              <Link className="Link" id="home" to="/">Home</Link>
+              <Link className="Link" id="reg" to="/Reg">Register</Link>
+              
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Navbar>
         </nav>
         <Route exact path="/" render={() => {
           return <div className="page-wrapper">
-            <Tasks tareas={this.state.tasks}/><Button></Button>
-            
+            <Tasks tareas={this.state.tasks}/>  
+            <Button variant="primary">Primary</Button>        
           </div> 
         }}> 
         </Route>
